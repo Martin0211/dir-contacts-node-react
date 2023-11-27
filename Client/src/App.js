504b17864b32c8e2969b4d745df7e80c62fab862
Martín0211 <martin021191@gmail.com>
2023-11-27 17:39:49 -0600

    Actualizar componente App con nuevas rutas

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import UserPage from './view/HomePage/UserHomePage';
+import ContactsPage from './view/ContactPage/Contactspage';
 
-function App() {
+
+function App(props) {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<UserPage />} />
+        <Route path="/contacts/:id" element={<ContactsPage />} />
+
+      </Routes>
+    </>
   );
 }
 
