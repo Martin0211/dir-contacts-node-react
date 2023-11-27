d0d505a62ca3a5f6d3caf35f7696397291a2ae55
Martín0211 <martin021191@gmail.com>
2023-11-27 17:43:55 -0600

    se crearon la view de la aplicacion

---
+++ Client/src/view/ContactPage/Contactspage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CardsConteinerContact from '../../components/CardsConteiner/CardsConteinerContact';
+//import styled from 'styled-components';
+
+const ContactsPage = (props) => {
+    return (
+        <div>
+            <div>
+                <CardsConteinerContact/>
+            </div>
+        </div>
+    );
+}
+
+export default ContactsPage;
---
+++ Client/src/view/HomePage/UserHomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CardsContainerUsers from '../../components/CardsConteiner/CardsContainerUser';
+//import styled from 'styled-components';
+
+const UserPage = (props) => {
+    return (
+        <div>
+            <div>
+                <CardsContainerUsers/>
+            </div>
+        </div>
+    );
+}
+
+export default UserPage;
