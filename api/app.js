49e4e393c1d99c243f17af8aa485106af9464c79
Martín0211 <martin021191@gmail.com>
2023-11-27 17:28:25 -0600

    Agregado middleware CORS a la aplicación Express

--- api/app.js
+++ api/app.js
@@ -1,6 +1,10 @@
 const express = require ("express")
 const routes = require("./src/router/index")
+const cors = require('cors');
+
 const app = express()
+
+app.use(cors());
 
 app.use(express.json());
 
