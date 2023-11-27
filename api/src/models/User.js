8cdec1bde938cf3b50060e88555ac9aa87fbf2f1
Martín0211 <martin021191@gmail.com>
2023-11-26 23:44:49 -0600

    Refactorizar modelo de User, eliminando campos no utilizados y relaciones, simplificando su estructura

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -1,7 +1,5 @@
 const { DataTypes } = require('sequelize');
-const { v4: uuidv4 } = require('uuid');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
   // defino el modelo
   return sequelize.define('User', {
@@ -14,17 +12,5 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
   });
 };
-
-  // Establece la relación uno a muchos con Contact
-  /* User.hasMany(Contact, { foreignKey: 'userId' }); */
