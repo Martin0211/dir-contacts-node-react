eb9798a5dc6367d238a7af992095a20a4ec91609
Martín0211 <martin021191@gmail.com>
2023-11-27 17:31:49 -0600

    Cambiar 'force' de true a false en la sincronización de la base de datos

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const app = require("./app")
 const {conn} = require("./src/models/index")
 
 conn.sync({
-    force: true
+    force: false
 }).then(() => {
     app.listen(3001, () => {
         console.log("Servidor funcionando en el puerto 3001");
