56874e8b5d11e00e5e297fc6e118191513386c0e
Martín0211 <martin021191@gmail.com>
2023-11-27 17:43:15 -0600

    se creo u configuro un redux

---
+++ Client/src/redux/actions.js
@@ -0,0 +1,116 @@
+import axios from 'axios'
+
+export const GET_USER_ALL = "GET_USER_ALL"
+export const POST_CREATE_USER = "POST_CREATE_USER"
+export const DELETE_USER = "DELETE_USER"
+export const PUT_UPDATE_USER = "PUT_UPDATE_USER"
+
+export const GET_CONTACT_BY_USER_ID = "GET_CONTACT_BY_USER_ID"
+export const POST_CREATE_CONTACT = "POST_CREATE_CONTACT"
+export const PUT_UPDATE_CONTACT = "PUT_UPDATE_CONTACT"
+export const DELETE_CONTACT = "DELETE_CONTACT"
+
+
+export const getUserAll = () => {
+    return async function (dispatch) {
+        try {
+            const apiData = await axios.get(`http://localhost:3001/api/user/`);
+            const user = apiData.data;
+            dispatch({ type: GET_USER_ALL, payload: user });
+
+        } catch (error) {
+            window.alert(error)
+        }
+
+    }
+};
+
+export const postCreateUser = (formData) => {
+    return async function (dispatch) {
+        try {
+            const apiData = await axios.post(`http://localhost:3001/api/user/`, formData);
+            const createUser = apiData.data;
+            dispatch({ type: POST_CREATE_USER, payload: createUser });
+
+        } catch (error) {
+            window.alert(error)
+        }
+
+
+    }
+};
+
+export const deleteUser = (id) => {
+    return async function (dispatch) {
+        try {
+            await axios.delete(`http://localhost:3001/api/user/${id}`);
+            dispatch({ type: DELETE_USER, payload: id });
+        } catch (error) {
+            window.alert(error);
+        }
+    }
+}
+
+export const putUpdateUser = (id, username) => {
+    return async function (dispatch) {
+        try {
+            const apiData = await axios.put(`http://localhost:3001/api/user/${id}`, { username });
+            const updatedUser = apiData.data; 
+            dispatch({ type: PUT_UPDATE_USER, payload: updatedUser });
+        } catch (error) {
+            window.alert(error);
+        }
+    }
+};
+
+export const getContactByUserId = (id) => {
+    return async function (dispatch) {
+        try {
+            const apiData = await axios.get(`http://localhost:3001/api/contac/contacts/${id}`);
+            const contacts = apiData.data;
+            dispatch({ type: GET_CONTACT_BY_USER_ID, payload: contacts });
+
+        } catch (error) {
+            window.alert(error)
+        }
+
+    }
+};
+
+export const postCreateContact = (id, formData) => {
+    return async function (dispatch) {
+        try {
+            const apiData = await axios.post(`http://localhost:3001/api/contac/${id}`, formData);
+            const createContact = apiData.data;
+            dispatch({ type: POST_CREATE_CONTACT, payload: createContact });
+
+        } catch (error) {
+            window.alert(error)
+        }
+
+
+    }
+};
+
+export const deleteContact = (userId,contactId) => {
+    return async function (dispatch) {
+        try {
+            await axios.delete(`http://localhost:3001/api/contac/${userId}/contacts/${contactId}`);
+            dispatch({ type: DELETE_CONTACT, payload: userId,contactId });
+        } catch (error) {
+            window.alert(error);
+        }
+    }
+};
+
+export const putUpdate = (userId,contactId, formData) => {
+    return async function (dispatch) {
+        try {
+            const apiData = await axios.put(`http://localhost:3001/api/contac/${userId}/contacts/${contactId}`, formData);
+            const updatedcontact = apiData.data;
+            dispatch({ type: PUT_UPDATE_CONTACT, payload: updatedcontact});
+        } catch (error) {
+            window.alert(error);
+        }
+    }
+};
---
+++ Client/src/redux/reducer.js
@@ -0,0 +1,50 @@
+import { GET_USER_ALL, POST_CREATE_USER, DELETE_USER, PUT_UPDATE_USER, GET_CONTACT_BY_USER_ID, POST_CREATE_CONTACT, PUT_UPDATE_CONTACT, DELETE_CONTACT } from './actions';
+
+const initialState = {
+    users: [],
+    contacts: [],
+    usersLoaded: false,
+    contactLoaded: false,
+};
+
+
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_USER_ALL:
+            return { ...state, users: action.payload, usersLoaded: true };
+
+        case POST_CREATE_USER:
+            return { ...state, users: [...state.users, action.payload] };
+
+        case DELETE_USER:
+            const updatedUsersDelete = state.users.filter(user => user.id !== action.payload);
+            return { ...state, users: updatedUsersDelete };
+
+        case PUT_UPDATE_USER:
+            const updatedUsers = state.users.map(user =>
+                user.id === action.payload.id ? { ...user, username: action.payload.username } : user
+            );
+            return { ...state, users: updatedUsers };
+
+        case GET_CONTACT_BY_USER_ID:
+            return { ...state, contacts: action.payload, contactLoaded: true };
+
+        case POST_CREATE_CONTACT:
+            return { ...state, contacts: [...state.contacts, action.payload] };
+
+        case DELETE_CONTACT:
+            const updatedContactsDelete = state.contacts.filter(contact => contact.id !== action.payload.contactId);
+            return { ...state, contacts: updatedContactsDelete };
+
+        case PUT_UPDATE_CONTACT:
+            const contactIndex = state.contacts.findIndex(contact => contact.id === action.payload.id);
+            const updatedContacts = [...state.contacts];
+            updatedContacts[contactIndex] = action.payload;
+            return { ...state, contacts: updatedContacts };
+
+        default:
+            return { ...state };
+    }
+};
+
+export default rootReducer;
