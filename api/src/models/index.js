f20e25748e4744c8c19cbe8c83256dc7a9cf3fe4
Martín0211 <martin021191@gmail.com>
2023-11-27 22:33:18 -0600

    modificacion en BD

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -4,7 +4,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT} = require("../../config
 const userFactory = require("./User");
 const contactFactory = require("./Contac");
 
-const sequelize = new Sequelize(`postgres://contact_directory_postgres_user:keo5wkLMhFnfSswiQcNlI0Wr4KKOoNAq@dpg-clijj51e313s73agahsg-a/contact_directory_postgres}`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
     logging: false,
     native: false,
 });
