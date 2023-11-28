01479e749bbcaef10e6a819865ca5ae93a7b798f
Martín0211 <martin021191@gmail.com>
2023-11-27 19:25:30 -0600

    configuracion de DB_PORT

--- api/config.js
+++ api/config.js
@@ -3,5 +3,6 @@ module.exports = {
     DB_PASSWORD: process.env.DB_PASSWORD || "admin",
     DB_HOST: process.env.DB_HOST || "localhost",
     DB_NAME: process.env.DB_NAME || "contact_directory_dev",
+    DB_PORT: process.env.DB_PORT || 5432,
     PORT: process.env.PORT || 3001,
   };
--- api/src/models/index.js
+++ api/src/models/index.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = require("../../config");
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT} = require("../../config");
 const userFactory = require("./User");
 const contactFactory = require("./Contac");
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
     logging: false,
     native: false,
 });
